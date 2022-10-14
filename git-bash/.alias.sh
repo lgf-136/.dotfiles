2bc94160ccc407867f41cc90db3602e9377d2e91
lgf <lgf_136@163.com>
2022-10-15 00:56:28 +0800

    ✏️ feat: change git-bash

--- git-bash/.alias.sh
+++ git-bash/.alias.sh
@@ -34,3 +34,15 @@ alias cu='su - lgf'
 alias cr='su -root'
 
 alias ep='explorer.exe .'
+
+alias update='cd ~/.dotfiles && git pull && cd .. && \
+              rm -f ~/.zshrc ~/.bashrc ~/.common.sh ~/.exports.sh ~/.alias.sh ~/.functions.sh ~/.gitconfig && \
+              ln -s -f ~/.dotfiles/git-bash/.zshrc  ~/.zshrc && \
+              ln -s -f ~/.dotfiles/git-bash/.bashrc  ~/.bashrc && \
+              ln -s -f ~/.dotfiles/git-bash/.common.sh  ~/.common.sh && \
+              ln -s -f ~/.dotfiles/git-bash/.exports.sh  ~/.exports.sh && \
+              ln -s -f ~/.dotfiles/git-bash/.alias.sh  ~/.alias.sh && \
+              ln -s -f ~/.dotfiles/git-bash/.functions.sh  ~/.functions.sh && \
+              ln -s -f ~/.dotfiles/git-bash/.gitconfig  ~/.gitconfig
+            '
+            
