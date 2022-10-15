2fc025bc1925672513201de75e40e28594ef7124
lgf <lgf_136@163.com>
2022-10-16 00:59:19 +0800

    🔧 fix: change alias.sh

--- msys2/.exports.sh
+++ msys2/.exports.sh
@@ -2,4 +2,4 @@
 
 . ~/.common.sh
 
-export PATH=$PATH:/bin:/usr/bin:/usr/local/bin:/c/Users/$(whoami)/.cargo/bin:~/.software/nvim-win64/bin:~/.software/nodejs
+export PATH=$PATH:/bin:/usr/bin:/usr/local/bin:/c/Users/$(whoami)/.cargo/bin:~/.software/nvim-win64/bin:~/.software/nodejs:~/.software/nodejs/nvm
