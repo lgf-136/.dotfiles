8fe69cfdcbd1243b0c7d7ba090ec51d6e654416d
lgf <lgf_136@163.com>
2022-10-15 13:30:28 +0800

    🔧 fix: add exports

--- msys2/.exports.sh
+++ msys2/.exports.sh
@@ -2,4 +2,4 @@
 
 . ~/.common.sh
 
-export PATH=$PATH:/bin:/usr/bin:/usr/local/bin:/c/Users/$(whoami)/.cargo/bin
+export PATH=$PATH:/bin:/usr/bin:/usr/local/bin:/c/Users/$(whoami)/.cargo/bin:/d/green_software/nvim-win64/bin
--- msys2/setup.sh
+++ msys2/setup.sh
@@ -4,10 +4,16 @@ pacman -Syyu
 
 pacman -S --noconfirm base-devel mingw-w64-x86_64-toolchain git bash zsh csh ksh tmux fish vim emacs nano python python3-pip perl ruby
 # pacman -S --noconfirm msys/make msys/cmake msys/automake msys/gcc msys/g++ msys/gdb
-pacman -S --noconfirm mingw-w64-x86_64-make mingw-w64-x86_64-cmake mingw-w64-x86_64-gcc mingw-w64-x86_64-gdb
+pacman -S --noconfirm mingw-w64-x86_64-make mingw-w64-x86_64-cmake mingw-w64-x86_64-gcc mingw-w64-x86_64-gdb mingw-w64-x86_64-openssl
 pacman -S --noconfirm mingw-w64-x86_64-fzf mingw-w64-x86_64-bat
 pacman -S --noconfirm mingw-w64-x86_64-julia mingw-w64-x86_64-go mingw-w64-x86_64-rust
 
+pacman -Sy
+pacman -S --needed filesystem msys2-runtime bash libreadline libiconv libarchive libgpgme libcurl pacman ncurses libintl
+pacman -S mingw-w64-x86_64-toolchain
+pacman -S mingw-w64-i686-toolchain
+pacman -S base-devel
+pacman -S vim
 
 # https://github.com/zellij-org/zellij
 # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
