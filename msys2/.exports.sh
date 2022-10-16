94ab3add01643916d16146b9084b13b6f06719a8
lgf <lgf_136@163.com>
2022-10-16 12:29:37 +0800

    🔧 fix: change msys2 exports.sh

--- msys2/.exports.sh
+++ msys2/.exports.sh
@@ -2,4 +2,4 @@
 
 . ~/.common.sh
 
-export PATH=$PATH:/bin:/usr/bin:/usr/local/bin:/c/Users/$(whoami)/.cargo/bin:~/.software/nvim-win64/bin:~/.software/nodejs:~/.software/nodejs/nvm
+export PATH=$PATH:/bin:/usr/bin:/usr/local/bin:/mingw64/bin:/clang64/bin:/ucrt64/bin:/c/Users/$(whoami)/.cargo/bin:~/.software/nvim-win64/bin:~/.software/nodejs:~/.software/nodejs/nvm
