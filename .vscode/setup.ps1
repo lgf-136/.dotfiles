d80ddc0cf6a7edd2cca9ba01dee3b98d12e2f4e4
lgf <lgf_136@163.com>
2022-10-18 00:59:15 +0800

    ✏️ feat: add vscode snippets

--- .vscode/setup.ps1
+++ .vscode/setup.ps1
@@ -3,4 +3,4 @@
 ln-s C:\Users\13662\.dotfiles\.vscode\settings.json C:\Users\13662\AppData\Roaming\Code\User\settings.json
 ln-s C:\Users\13662\.dotfiles\.vscode\keybindings.json C:\Users\13662\AppData\Roaming\Code\User\keybindings.json
 
-
+ln-s C:\Users\13662\.dotfiles\.vscode\snippets C:\Users\13662\AppData\Roaming\Code\User\snippets
