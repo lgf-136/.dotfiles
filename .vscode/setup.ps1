7e43d0a38ede999fdc27fe920642b1acf8341b4b
lgf <lgf_136@163.com>
2022-10-18 00:42:39 +0800

    ✏️ feat: add vscode config

---
+++ .vscode/setup.ps1
@@ -0,0 +1,6 @@
+
+# 以管理员运行
+ln-s C:\Users\13662\.dotfiles\.vscode\settings.json C:\Users\13662\AppData\Roaming\Code\User\settings.json
+ln-s C:\Users\13662\.dotfiles\.vscode\keybindings.json C:\Users\13662\AppData\Roaming\Code\User\keybindings.json
+
+
