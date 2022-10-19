d4b62713a92c7ebd3dc204c3061333e600530858
lgf <lgf_136@163.com>
2022-10-19 16:53:58 +0800

    🔧 fix: change exports.sh

--- .exports.sh
+++ .exports.sh
@@ -2,4 +2,4 @@
 
 . ~/.common.sh
 
-export PATH=$PATH:/bin:/usr/bin:/usr/local/bin:/mnt/c/Windows:/mnt/c/Program\ Files/Microsoft\ VS\ Code/bin
+export PATH=$PATH:$HOME/.local/bin:/bin:/usr/bin:/usr/local/bin:/mnt/c/Windows:/mnt/c/Program\ Files/Microsoft\ VS\ Code/bin
