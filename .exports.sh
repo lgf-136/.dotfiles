f69bce6b92a6bce07e47139c141863f777ad7f6c
lgf <lgf_136@163.com>
2022-10-20 00:11:16 +0800

    🔧 fix: add exports

--- .exports.sh
+++ .exports.sh
@@ -3,3 +3,8 @@
 . ~/.common.sh
 
 export PATH=$PATH:$HOME/.local/bin:/bin:/usr/bin:/usr/local/bin:/mnt/c/Windows:/mnt/c/Program\ Files/Microsoft\ VS\ Code/bin
+
+export PYENV_ROOR="$HOME/.pyenv"
+export PATH=~/.pyenv/libexec:$PATH
+# eval "$(pyenv init -)"
+# eval "$(pyenv virtualenv-init -)"
